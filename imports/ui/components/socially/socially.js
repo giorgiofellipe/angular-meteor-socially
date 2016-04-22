db3cbdaf6b77d024a2aebd098b27886a250756cc
Giorgio Fellipe <giorgiofellipe@gmail.com>
2016-04-22 11:38:56 -0300

    Add routing

--- imports/ui/components/socially/socially.js
+++ imports/ui/components/socially/socially.js
@@ -1,8 +1,10 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import uiRouter from 'angular-ui-router';
 
 import './socially.html';
 import { name as PartiesList } from '../partiesList/partiesList';
+import { name as Navigation } from '../navigation/navigation';
 
 class Socially {}
 
@@ -11,9 +13,20 @@ const name = 'socially';
 // create a module
 export default angular.module(name, [
   angularMeteor,
+  uiRouter,
+  Navigation,
   PartiesList
 ]).component(name, {
   templateUrl: `imports/ui/components/${name}/${name}.html`,
   controllerAs: name,
   controller: Socially
-});
+})
+.config(config);
+
+function config($locationProvider, $urlRouterProvider) {
+  'ngInject';
+
+  $locationProvider.html5Mode(true);
+
+  $urlRouterProvider.otherwise('/parties');
+}
