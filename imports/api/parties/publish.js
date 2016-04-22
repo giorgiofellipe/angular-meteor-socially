8166ed4151513a13642d62f6cc374c147a452be9
Giorgio Fellipe <giorgiofellipe@gmail.com>
2016-04-22 14:31:24 -0300

    Add pagination

--- imports/api/parties/publish.js
+++ imports/api/parties/publish.js
@@ -1,9 +1,10 @@
 import { Meteor } from 'meteor/meteor';
+import { Counts } from 'meteor/tmeasday:publish-counts';
 
 import { Parties } from './collection';
 
 if (Meteor.isServer) {
-  Meteor.publish('parties', function() {
+  Meteor.publish('parties', function(options) {
     const selector = {
       $or: [{
         // the public parties
@@ -25,7 +26,11 @@ if (Meteor.isServer) {
         }]
       }]
     };
+    
+    Counts.publish(this, 'numberOfParties', Parties.find(selector), {
+      noReady: true
+    });
 
-    return Parties.find(selector);
+    return Parties.find(selector, options);
   });
 }
--- imports/ui/components/partiesList/partiesList.js
+++ imports/ui/components/partiesList/partiesList.js
@@ -1,6 +1,8 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import uiRouter from 'angular-ui-router';
+import utilsPagination from 'angular-utils-pagination';
+import { Counts } from 'meteor/tmeasday:publish-counts';
 
 import './partiesList.html';
 import { Parties } from '../../../api/parties';
@@ -12,14 +14,34 @@ class PartiesList {
     'ngInject';
 
     $reactive(this).attach($scope);
-    
-    this.subscribe('parties');
+
+    this.autoHide = false;
+    this.perPage = 3;
+    this.page = 1;
+    this.sort = {
+      name: 1
+    };
+
+    this.subscribe('parties', () => [{
+      limit: parseInt(this.perPage),
+      skip: parseInt((this.getReactively('page') - 1) * this.perPage),
+      sort: this.getReactively('sort')}
+    ]);
 
     this.helpers({
       parties() {
-        return Parties.find({});
+        return Parties.find({}, {
+          sort : this.getReactively('sort')
+        });
+      },
+      partiesCount() {
+        return Counts.get('numberOfParties');
       }
     });
+  }
+
+  pageChanged(newPage) {
+    this.page = newPage;
   }
 }
 
@@ -29,6 +51,7 @@ const name = 'partiesList';
 export default angular.module(name, [
   angularMeteor,
   uiRouter,
+  utilsPagination,
   PartyAdd,
   PartyRemove
 ]).component(name, {
